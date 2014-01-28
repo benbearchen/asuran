e5c23bfc0f52d4d197be3e05112f5785751c9329
BenBear <benbearchen@gmail.com>
2014-01-28 12:04:46 +0800

    MatchPath match complete path node

--- net/httpd/util.go
+++ net/httpd/util.go
@@ -5,7 +5,11 @@ import (
 )
 
 func MatchPath(path, match string) (string, bool) {
-	if strings.HasPrefix(path, match) {
+	if path == match {
+		return "", true
+	}
+
+	if strings.HasPrefix(path, match+"/") {
 		return path[len(match):], true
 	} else {
 		return "", false
