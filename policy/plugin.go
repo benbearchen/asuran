c516a722c31aed3ea5d5c388736aa8f5e610aa13
benbearchen <benbearchen@gmail.com>
2016-03-19 01:06:25 +0800

    PluginPolicy has setting now

--- policy/plugin.go
+++ policy/plugin.go
@@ -1,19 +1,86 @@
 package policy
 
+import (
+	"fmt"
+	"strings"
+)
+
 const pluginKeyword = "plugin"
 
+const settingSubKeyword = "setting"
+
 type PluginPolicy struct {
-	stringPolicy
+	setting string
+	name    string
+}
+
+type pluginPolicyFactory struct {
 }
 
 func init() {
-	regFactory(newStringPolicyFactory(pluginKeyword, "external plugin name", func(name string) (Policy, error) {
-		return &PluginPolicy{stringPolicy{pluginKeyword, name, func(name string) string {
-			return "通过插件 " + name + " 处理"
-		}}}, nil
-	}))
+	regFactory(new(pluginPolicyFactory))
+}
+
+func (f *pluginPolicyFactory) Keyword() string {
+	return pluginKeyword
+}
+
+func (f *pluginPolicyFactory) Build(args []string) (Policy, []string, error) {
+	if len(args) <= 0 {
+		return nil, args, fmt.Errorf("need atleast a external plugin name")
+	}
+
+	setting := ""
+	if args[0] == settingSubKeyword {
+		if len(args) < 3 {
+			return nil, args, fmt.Errorf("plugin setting need a setting-string, then a plugin name")
+		}
+
+		setting = args[1]
+		args = args[2:]
+	}
+
+	name := args[0]
+	args = args[1:]
+
+	return &PluginPolicy{setting, name}, args, nil
+}
+
+func (p *PluginPolicy) Keyword() string {
+	return pluginKeyword
+}
+
+func (p *PluginPolicy) Command() string {
+	cmds := make([]string, 0, 4)
+	cmds = append(cmds, pluginKeyword)
+	if len(p.setting) > 0 {
+		cmds = append(cmds, settingSubKeyword, p.setting)
+	}
+
+	cmds = append(cmds, p.name)
+	return strings.Join(cmds, " ")
+}
+
+func (p *PluginPolicy) Comment() string {
+	return "通过插件 " + p.name + " 处理"
+}
+
+func (p *PluginPolicy) Update(n Policy) error {
+	switch n := n.(type) {
+	case *PluginPolicy:
+		p.setting = n.setting
+		p.name = n.name
+	default:
+		return fmt.Errorf("unmatch policy")
+	}
+
+	return nil
 }
 
 func (p *PluginPolicy) Name() string {
-	return p.Value()
+	return p.name
+}
+
+func (p *PluginPolicy) Setting() string {
+	return p.setting
 }
--- web/proxy/plugin/chaosjson/chaosjson.go
+++ web/proxy/plugin/chaosjson/chaosjson.go
@@ -4,8 +4,15 @@ import (
 	helper "github.com/benbearchen/asuran/net"
 	"github.com/benbearchen/asuran/web/proxy/plugin/api"
 
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
+)
+
+var (
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
 func init() {
@@ -23,12 +30,121 @@ func chaosjson(context *api.Context, targetURI string, w http.ResponseWriter, r 
 	defer response.Close()
 
 	bytes, err := response.ReadAllBytes()
+	if err == nil {
+		bytes, err = dealJson(context.Policy.Setting(), bytes)
+	}
+
 	if err != nil {
 		w.WriteHeader(502)
 		fmt.Fprintln(w, err)
 		return
 	}
 
-	bytes = bytes[:len(bytes)/2]
 	w.Write(bytes)
+}
+
+func dealJson(setting string, bytes []byte) ([]byte, error) {
+	switch setting {
+	case "none":
+		return []byte{}, nil
+	case "empty-object":
+		return []byte("{}"), nil
+	case "empty-array":
+		return []byte("[]"), nil
+	}
+
+	var obj interface{}
+	err := json.Unmarshal(bytes, &obj)
+	if err != nil {
+		return nil, err
+	}
+
+	switch setting {
+	case "eat-any-one":
+		obj = eatJson(obj, 1)
+	case "change-any-one":
+		obj = changeJson(obj, 1)
+	}
+
+	return json.Marshal(obj)
+}
+
+func eatJson(obj interface{}, times int) interface{} {
+	switch obj := obj.(type) {
+	case bool, float64, string:
+		return nil
+	case []interface{}:
+		if len(obj) == 0 {
+			return nil
+		}
+
+		i := r.Int() % len(obj)
+		newly := make([]interface{}, i, len(obj))
+		copy(newly, obj[0:i])
+		return append(newly, obj[i+1:]...)
+	case map[string]interface{}:
+		if len(obj) == 0 {
+			return nil
+		}
+
+		newly := make(map[string]interface{})
+		i := r.Int() % len(obj)
+		for k, v := range obj {
+			if i != 0 {
+				newly[k] = v
+			}
+
+			i--
+		}
+
+		return newly
+	default:
+		return nil
+	}
+}
+
+func changeJson(obj interface{}, times int) interface{} {
+	switch obj := obj.(type) {
+	case bool:
+		return !obj
+	case float64:
+		return obj + 1
+	case string:
+		if obj == "" {
+			return "CCCCCCCC"
+		} else {
+			return ""
+		}
+	case []interface{}:
+		if len(obj) == 0 {
+			return []interface{}{0}
+		}
+
+		i := r.Int() % len(obj)
+		newly := make([]interface{}, i, len(obj))
+		copy(newly, obj[0:i])
+		newly = append(newly, changeJson(obj[i], times-1))
+		newly = append(newly, obj[i+1:]...)
+		return newly
+	case map[string]interface{}:
+		if len(obj) == 0 {
+			return make(map[string]interface{})
+		}
+
+		newly := make(map[string]interface{})
+		i := r.Int() % len(obj)
+		for k, v := range obj {
+			if i == 0 {
+				newly[k] = changeJson(v, times-1)
+			} else {
+				newly[k] = v
+			}
+
+			i--
+		}
+
+		return newly
+	default:
+		return nil
+	}
 }
