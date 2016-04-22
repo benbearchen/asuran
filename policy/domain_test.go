663b0d5420da19305dc3a4fa742801d41d109028
benbearchen <benbearchen@gmail.com>
2016-04-22 21:35:40 +0800

    Add test for domain policy opts
    domain shuffle, n, circular

--- policy/domain_test.go
+++ policy/domain_test.go
@@ -44,3 +44,99 @@ func TestDomainPolicy(t *testing.T) {
 		}
 	}
 }
+
+func TestDomainPolicyOpts(t *testing.T) {
+	cmd := "domain n 1 g.cn 192.168.1.1,192.168.1.2"
+	d, err := Factory(cmd)
+	if err != nil {
+		t.Errorf("domain(%s) failed: ", cmd, err)
+	} else if d.Command() != cmd {
+		t.Errorf("domain(%s).Command() changed: %s", cmd, d.Command())
+	}
+
+	cmd = "domain g.cn shuffle"
+	d, err = Factory(cmd)
+	if err == nil {
+		t.Errorf("domain(%s) didn't detect error", cmd)
+	}
+
+	cmd = "domain shuffle g.cn 192.168.1.1,192.168.1.2"
+	d, err = Factory(cmd)
+	if err != nil {
+		t.Errorf("domain(%s) failed: ", cmd, err)
+	} else if d.Command() != cmd {
+		t.Errorf("domain(%s).Command() changed: %s", cmd, d.Command())
+	} else {
+		d := d.(*DomainPolicy)
+		found := false
+		for i := 0; i < 100; i++ {
+			ips := d.NextIPs()
+			if len(ips) == 2 && ips[0] == "192.168.1.2" && ips[1] == "192.168.1.1" {
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			t.Errorf("domain(%s).NextIPs() maybe not shuffled", cmd)
+		}
+	}
+
+	cmd = "domain n -1 g.cn"
+	d, err = Factory(cmd)
+	if err == nil {
+		t.Errorf("domain(%s) didn't detect error", cmd)
+	}
+
+	cmd = "domain n 0 g.cn 192.168.1.1,192.168.1.2"
+	d, err = Factory(cmd)
+	if err != nil {
+		t.Errorf("domain(%s) failed: ", cmd, err)
+	} else if d.Command() != cmd {
+		t.Errorf("domain(%s).Command() changed: %s", cmd, d.Command())
+	} else {
+		d := d.(*DomainPolicy)
+		ips := d.NextIPs()
+		if len(ips) != 0 {
+			t.Errorf("domain(%s).NextIPs() return not empty: %v", cmd, ips)
+		}
+	}
+
+	cmd = "domain n 1 g.cn 192.168.1.1,192.168.1.2"
+	d, err = Factory(cmd)
+	if err != nil {
+		t.Errorf("domain(%s) failed: ", cmd, err)
+	} else if d.Command() != cmd {
+		t.Errorf("domain(%s).Command() changed: %s", cmd, d.Command())
+	} else {
+		d := d.(*DomainPolicy)
+		ips := d.NextIPs()
+		if len(ips) != 1 {
+			t.Errorf("domain(%s).NextIPs() return not one ip: %v", cmd, ips)
+		}
+	}
+
+	cmd = "domain n 1 circular g.cn 192.168.1.1,192.168.1.2"
+	d, err = Factory(cmd)
+	if err != nil {
+		t.Errorf("domain(%s) failed: ", cmd, err)
+	} else {
+		d := d.(*DomainPolicy)
+		ips := d.NextIPs()
+		if len(ips) != 1 {
+			t.Errorf("domain(%s).NextIPs() return not one ip: %v", cmd, ips)
+		} else if ips[0] != "192.168.1.1" {
+			t.Errorf("domain(%s).NextIPs() return not [192.168.1.1]: %v", cmd, ips)
+		}
+
+		ips = d.NextIPs()
+		if len(ips) != 1 || ips[0] != "192.168.1.2" {
+			t.Errorf("domain(%s).NextIPs() return not [192.168.1.2]: %v", cmd, ips)
+		}
+
+		ips = d.NextIPs()
+		if len(ips) != 1 || ips[0] != "192.168.1.1" {
+			t.Errorf("domain(%s).NextIPs() return not [192.168.1.1]: %v", cmd, ips)
+		}
+	}
+}
